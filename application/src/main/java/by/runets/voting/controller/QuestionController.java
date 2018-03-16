20fe4de241b5da6abaa6fe0182d1efaac396cfb9
Kirill Runets <rkmwork97@gmail.com>
2018-03-16 11:03:30 +0300

    Add REST representation of question

--- application/src/main/java/by/runets/voting/controller/QuestionController.java
+++ application/src/main/java/by/runets/voting/controller/QuestionController.java
@@ -1,16 +1,15 @@
 package by.runets.voting.controller;
 
 import by.runets.voting.dto.QuestionDTO;
+import by.runets.voting.exception.ResourceNotFoundException;
 import by.runets.voting.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,4 +24,39 @@ public class QuestionController {
         return ResponseEntity.ok(questionDTO);
     }
 
+    @RequestMapping(value = "/question/{id}/disable", method = RequestMethod.PUT)
+    public ResponseEntity<QuestionDTO> disable(@RequestBody @Valid final QuestionDTO questionDTO){
+        questionDTO.setEnabled(false);
+        questionService.save(questionDTO);
+        return ResponseEntity.ok(questionDTO);
+    }
+
+    @RequestMapping(value = "/question/{id}/delete", method = RequestMethod.DELETE)
+    public ResponseEntity<QuestionDTO> delete(@PathVariable int id) {
+        try {
+            questionService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (ResourceNotFoundException e) {
+            log.error(e);
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @RequestMapping(value = "/question/{id}", method = RequestMethod.GET)
+    public ResponseEntity<QuestionDTO> find(@PathVariable int id){
+        try {
+            QuestionDTO questionDTO = questionService.find(id);
+            return ResponseEntity.ok(questionDTO);
+        } catch (ResourceNotFoundException e) {
+            log.error(e);
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @RequestMapping(value = "/question/all", method = RequestMethod.GET)
+    public ResponseEntity<List<QuestionDTO>> findAll(){
+        List<QuestionDTO> questions = questionService.findAll();
+        return ResponseEntity.ok(questions);
+    }
 }
